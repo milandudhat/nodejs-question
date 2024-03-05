18afffe7fe9b865cd215b99c01f0338835f2b293
smit dudhat <milandudhat2828@gmail.com>
2024-03-05 17:01:54 +0530

    init

---
+++ q2.js
@@ -0,0 +1,36 @@
+const products = [
+    { "id": 1, "sku": "abc", "productName": "name 1", "category": 1 },
+    { "id": 2, "sku": "def", "productName": "name 2", "category": 2 },
+    { "id": 3, "sku": "ghi", "productName": "name 1", "category": 2 },
+    { "id": 4, "sku": "klm", "productName": "name 1", "category": 3 },
+    { "id": 5, "sku": "xyz", "productName": "name 1", "category": 1 }
+  ];
+  
+  const pricing = [
+    { "sku": "abc", "price": 10 },
+    { "sku": "def", "price": 20 },
+    { "sku": "ghi", "price": 30 },
+    { "sku": "klm", "price": 40 },
+    { "sku": "xyz", "price": 50 }
+  ];
+  
+  const categories = [
+    { "id": 1, "name": "category 1" },
+    { "id": 2, "name": "category 2" },
+    { "id": 3, "name": "category 3" },
+    { "id": 4, "name": "category 4" },
+    { "id": 5, "name": "category 5" }
+  ];
+  
+  const pricingMap = new Map(pricing.map(item => [item.sku, item.price]));
+  
+  const resultArray = products.map(product => ({
+    id: product.id,
+    sku: product.sku,
+    productName: product.productName,
+    category: product.category,
+    price: pricingMap.get(product.sku)
+  }));
+  
+  console.log(resultArray);
+  
